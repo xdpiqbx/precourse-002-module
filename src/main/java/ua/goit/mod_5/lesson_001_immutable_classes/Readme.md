177532216b04a2b4cd06853de9aa71cdcbf79be3
xdpiqbx <xdpiqbx@gmail.com>
2022-05-11 22:00:44 +0300

    module 6 lesson 2 done

--- src/main/java/ua/goit/mod_5/lesson_001_immutable_classes/Readme.md
+++ src/main/java/ua/goit/mod_5/lesson_001_immutable_classes/Readme.md
@@ -1,4 +1,8 @@
 ## Immutable
+- String
+> each change to an instance of a class results in the creation of a new object.
 
 ## Mutable
-- String
+> each change to an instance of a class does NOT result in the creation of a new object.
+- `StringBuilder` - contains unsynchronized methods (not for multi thread, be careful can beat dirty reading)
+- `StringBuffer` - contains synchronized methods
--- src/main/java/ua/goit/mod_5/lesson_002_string/StringDemo.java
+++ src/main/java/ua/goit/mod_5/lesson_002_string/StringDemo.java
@@ -18,7 +18,14 @@ public class StringDemo {
 //        substringExample();
 //        replaceExample();
 //        stringFormatExample();
-        stringJoinerExxample();
+
+//        System.out.println(reverseStringWithNewObject("Hello World"));
+//        System.out.println(reverseStringWithoutAdditionalMemoryUsage("Hello World $$$"));
+
+        System.out.println(stringBuilderReverseExample("String Builder Reverse Example"));
+
+        System.out.println(stringBufferReverseExample("String BUFFER Reverse Example"));
+
     }
     private static void getStringFromPool(){
         String first = "Hello World";
@@ -69,7 +76,34 @@ public class StringDemo {
         }
         System.out.println(joiner.toString());
     }
-    public static void reverseStringWithNewObject(){
-
+    public static String reverseStringWithNewObject(String input){
+        char[] inputChar = input.toCharArray();
+        char[] reversedChar = new char[inputChar.length];
+        for (int i = 0; i < inputChar.length; i++) {
+            reversedChar[i] = inputChar[inputChar.length - 1 - i];
+        }
+        return new String(reversedChar);
+    }
+    public static String reverseStringWithoutAdditionalMemoryUsage(String input){
+        char[] inputArray = input.toCharArray();
+        int begin = 0;
+        int end = inputArray.length - 1;
+        char temp;
+        while(begin < end){
+            temp = inputArray[begin];
+            inputArray[begin] = inputArray[end];
+            inputArray[end] = temp;
+            ++begin;
+            --end;
+        }
+        return new String(inputArray);
+    }
+    private static String stringBuilderReverseExample(String input){
+        StringBuilder builder = new StringBuilder();
+        return builder.append(input).reverse().toString();
+    }
+    private static String stringBufferReverseExample(String input){
+        StringBuffer builder = new StringBuffer();
+        return builder.append(input).reverse().toString();
     }
 }
