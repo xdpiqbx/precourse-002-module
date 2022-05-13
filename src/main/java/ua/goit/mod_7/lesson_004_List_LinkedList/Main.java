d39fd0ee61745c0acdef563846fbff20114de608
xdpiqbx <xdpiqbx@gmail.com>
2022-05-13 17:39:33 +0300

    started lesson 4 in module 7

---
+++ src/main/java/ua/goit/mod_7/lesson_004_List_LinkedList/Main.java
@@ -0,0 +1,12 @@
+package ua.goit.mod_7.lesson_004_List_LinkedList;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
+        List<String> list = new LinkedList<>();
+    }
+}
--- src/main/java/ua/goit/mod_7/lesson_004_List_LinkedList/Readme.md
+++ src/main/java/ua/goit/mod_7/lesson_004_List_LinkedList/Readme.md
@@ -1,1 +1,5 @@
 ![](https://i.stack.imgur.com/epdYb.png)
+
+![ArrayList vs LinkedList](https://qph.cf2.quoracdn.net/main-qimg-41cdfa9a815220598f2c03f1bccaeff8)
+
+[START FROM!!!](https://youtu.be/cGckmpP0hkY?t=881)
