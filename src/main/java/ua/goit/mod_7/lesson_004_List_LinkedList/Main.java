602397801b40635f5e98d3ddff793f169c736cf5
xdpiqbx <xdpiqbx@gmail.com>
2022-05-13 20:23:27 +0300

    module 7 lesson 4 done

--- src/main/java/ua/goit/mod_7/lesson_004_List_LinkedList/Main.java
+++ src/main/java/ua/goit/mod_7/lesson_004_List_LinkedList/Main.java
@@ -1,12 +1,38 @@
 package ua.goit.mod_7.lesson_004_List_LinkedList;
 
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Deque<String> deque = new LinkedList<>();
-        List<String> list = new LinkedList<>();
+        List<String> classB = new LinkedList<>();
+        classB.add("Sam");
+        classB.add("El");
+        List<String> classA = new LinkedList<>();
+        classA.add("Bill");
+        classA.add(0, "John"); // first in list classA
+        classA.add("Smith");
+
+//        classA.addAll(classB); // add to and
+        classA.addAll(0, classB); // add to start
+
+        System.out.println(
+            classA.contains("John") // true
+        );
+        System.out.println(
+            classA.contains("john") // false
+        );
+
+        System.out.println(classA);
+
+        classA.remove("John");
+
+        System.out.println(classA);
+
+        for (String node: classA) {
+            System.out.println(node);
+        }
+
+        System.out.println(classA.lastIndexOf("El"));
     }
 }
