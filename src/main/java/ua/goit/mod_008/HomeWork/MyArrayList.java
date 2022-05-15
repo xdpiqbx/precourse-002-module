2f7a5340c9d46db0303b0f14542bd5edd0f03c50
xdpiqbx <xdpiqbx@gmail.com>
2022-05-15 15:00:23 +0300

    Start HW MyArrList method add done

---
+++ src/main/java/ua/goit/mod_008/HomeWork/MyArrayList.java
@@ -0,0 +1,22 @@
+package ua.goit.mod_008.HomeWork;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MyArrayList<T> {
+
+    private T[] array = (T[]) new Object[0];
+    public MyArrayList(){
+
+    }
+    public boolean add(T item){
+        try{
+            this.array = Arrays.copyOf(this.array, this.array.length + 1);
+            this.array[array.length - 1] = item;
+            return true;
+        }catch (Exception e){
+            System.out.println(Arrays.toString(e.getStackTrace()));
+            return false;
+        }
+    }
+}
---
+++ src/main/java/ua/goit/mod_008/HomeWork/main.java
@@ -0,0 +1,31 @@
+package ua.goit.mod_008.HomeWork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+- `add(Object value)` добавляет элемент в конец
+- `remove(int index)` удаляет и возвращает элемент под индексом
+- `clear()` очищает коллекцию (залишиться порожній масив [])
+- `size()` возвращает размер коллекции array.length()
+- `get(int index)` возвращает элемент под индексом
+*/
+
+public class main {
+    public static void main(String[] args) {
+//        ArrayList<String> names = new ArrayList<>();
+//        names.add("Bill");
+//        names.add("John");
+//        System.out.println(names.remove(0));
+//        System.out.println(names.size());
+//        System.out.println(names.get(0));
+//        names.clear();
+
+        MyArrayList<String> nums = new MyArrayList<>();
+        nums.add("dfvfdv");
+        nums.add("aaaaa");
+        nums.add("ddddd");
+        nums.add("bbbb");
+        nums.add("errergereerg");
+    }
+}
