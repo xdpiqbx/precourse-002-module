1b99229b5a73bdb484c38ac4ae03bff040291668
xdpiqbx <xdpiqbx@gmail.com>
2022-05-16 18:06:20 +0300

    try harder =(

--- src/main/java/ua/goit/mod_008/HomeWork/MyLinkedList/MyLinkedList_RAW.java
+++ src/main/java/ua/goit/mod_008/HomeWork/MyLinkedList/MyLinkedList_RAW.java
@@ -1,22 +1,23 @@
 package ua.goit.mod_008.HomeWork.MyLinkedList;
-public class MyLinkedList<T> {
-    private Node<T> listHead;
-    private Node<T> listTail;
+
+public class MyLinkedList_RAW<T> {
+    private Node<T> listHeader; // *********************************************
     public void add(T data){
         Node<T> newNode = new Node<T>(data);
-        if(this.listHead == null){
-            this.listHead = newNode;
+        if(this.listHeader == null){
+            this.listHeader = newNode;
         }else{
-            Node<T> currentNode = this.listHead;
+            Node<T> currentNode = this.listHeader;
             while(currentNode.nextNode != null){
                 currentNode = currentNode.nextNode;
             }
             currentNode.nextNode = newNode;
         }
     }
+
     public void output() {
-        if(this.listHead != null){
-            Node<T> currentNode = this.listHead;
+        if(this.listHeader != null){
+            Node<T> currentNode = this.listHeader;
             while(currentNode.nextNode != null){
                 System.out.println(currentNode.data);
                 currentNode = currentNode.nextNode;
@@ -24,12 +25,13 @@ public class MyLinkedList<T> {
             System.out.println(currentNode.data);
         }
     }
+
     public static class Node<T> {
         public T data;
         public Node<T> nextNode;
-        public Node<T> prevNode;
         public Node(T data){
             this.data = data;
         }
     }
+
 }
