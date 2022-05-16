004534486223f668df8ff919a38ab6d024b505c0
xdpiqbx <xdpiqbx@gmail.com>
2022-05-16 17:32:43 +0300

    linked list return to start =(

--- src/main/java/ua/goit/mod_008/HomeWork/MyLinkedList/MyLinkedList.java
+++ src/main/java/ua/goit/mod_008/HomeWork/MyLinkedList/MyLinkedList.java
@@ -1,26 +1,23 @@
 package ua.goit.mod_008.HomeWork.MyLinkedList;
 
 public class MyLinkedList<T> {
-    private Node listHead; // *********************************************
-    private Node listTail; // *********************************************
+    private Node<T> listHeader; // *********************************************
     public void add(T data){
         Node<T> newNode = new Node<T>(data);
-        if(this.listHead == null){
-            this.listHead.nextNode = newNode.prevNode;
-            this.listTail.prevNode = newNode.nextNode;
+        if(this.listHeader == null){
+            this.listHeader = newNode;
         }else{
-            Node<T> currentNode = this.listHead;
+            Node<T> currentNode = this.listHeader;
             while(currentNode.nextNode != null){
                 currentNode = currentNode.nextNode;
             }
-            currentNode.nextNode = newNode.prevNode;
-            this.listTail.prevNode = newNode.nextNode;
+            currentNode.nextNode = newNode;
         }
     }
 
     public void output() {
-        if(this.listHead != null){
-            Node<T> currentNode = this.listHead;
+        if(this.listHeader != null){
+            Node<T> currentNode = this.listHeader;
             while(currentNode.nextNode != null){
                 System.out.println(currentNode.data);
                 currentNode = currentNode.nextNode;
@@ -32,9 +29,9 @@ public class MyLinkedList<T> {
     public static class Node<T> {
         public T data;
         public Node<T> nextNode;
-        public Node<T> prevNode;
         public Node(T data){
             this.data = data;
         }
     }
+
 }
--- src/main/java/ua/goit/mod_008/HomeWork/MyLinkedList/main.java
+++ src/main/java/ua/goit/mod_008/HomeWork/MyLinkedList/main.java
@@ -32,14 +32,15 @@ public class main {
 
         System.out.println("=== MyLinkedList ===");
 
-        MyLinkedList_MAIN<String> myList = new MyLinkedList_MAIN<>();
+        MyLinkedList<String> myList = new MyLinkedList<>();
         myList.add("first MY 001");
-        myList.add("first MY 002");
-        myList.add("first MY 002");
-        myList.add("first MY 002");
         myList.add("first MY 002");
         myList.add("first MY 003");
         myList.add("first MY 004");
+        myList.add("first MY 005");
+        myList.add("first MY 006");
+        myList.add("first MY 007");
         myList.output();
+        System.out.println("================");
     }
 }
