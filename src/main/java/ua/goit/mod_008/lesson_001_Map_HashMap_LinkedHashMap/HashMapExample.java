76b86c2a8d5b12a701230bbb89eba607158ae2d2
xdpiqbx <xdpiqbx@gmail.com>
2022-06-25 08:09:30 +0300

    hash map added methods

--- src/main/java/ua/goit/mod_008/lesson_001_Map_HashMap_LinkedHashMap/HashMapExample.java
+++ src/main/java/ua/goit/mod_008/lesson_001_Map_HashMap_LinkedHashMap/HashMapExample.java
@@ -13,26 +13,41 @@ public class HashMapExample {
         capitals.put("Ukraine", "Kyiv");
         capitals.put("Spain", "Madrid");
         capitals.put(null, "null value");
+        System.out.println("Capitals --------------------------");
         System.out.println(capitals); // {Ukraine=Kyiv, Germany=Berlin, Spain=Madrid}
 
-        // get element from map
-        String germany = capitals.get("Germany");
-        System.out.println(germany); // Berlin
+        // -------------------------- .get() element from map
+        System.out.println(".get()-----------------------------");
+        System.out.println(capitals.get("Germany")); // Berlin
 
 
-        // --------------------------
+        // -------------------------- .size()
+        System.out.println(".size()----------------------------");
         System.out.println("Size: " + capitals.size());
 
-        // --------------------------
-        Set<String> keySet = capitals.keySet();
+        // -------------------------- .keySet()
+        Set<String> keySet = capitals.keySet(); // ------------------- Set<String>
+        System.out.println(".keySet()--------------------------");
         System.out.println(keySet);
 
-        // --------------------------
-        Collection<String> values = capitals.values();
+        System.out.println(".keySet()--foreach-----------------");
+        for (String key: capitals.keySet()) {
+            String value = capitals.get(key);
+            System.out.printf("%s = %s\n", key, value);
+        }
+
+        // -------------------------- .values()
+        Collection<String> values = capitals.values(); // ------------ Collection<String>
+        System.out.println(".values()--------------------------");
         System.out.println(values);
 
+        System.out.println(".containsKey()-.containsValue()----");
+        System.out.println(capitals.containsKey("Ukraine"));
+        System.out.println(capitals.containsValue("Madrid"));
+
         // --------------------------
-        Set<Map.Entry<String, String>> entries = capitals.entrySet();
+        Set<Map.Entry<String, String>> entries = capitals.entrySet(); // Set<Map.Entry<String, String>>
+        System.out.println(".entrySet()------------------------");
         for (Map.Entry<String, String> entry : entries) {
             System.out.println("Entries ===> : ");
             System.out.println("HashCode: " + entry.hashCode());
@@ -40,8 +55,12 @@ public class HashMapExample {
             System.out.println("Value: " + entry.getValue());
         }
 
+
+
         // remove element from map
-        capitals.remove("Germany"); // {Ukraine=Kyiv, Spain=Madrid}
+        System.out.println(".remove()-------------------------");
+        System.out.println(capitals.remove("Germany")); // {Ukraine=Kyiv, Spain=Madrid}
+        System.out.println(".clear()--------------------------");
         capitals.clear(); // {}
         System.out.println(capitals);
     }
