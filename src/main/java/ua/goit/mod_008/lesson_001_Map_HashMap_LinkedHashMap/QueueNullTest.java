3453a147174ed30e96d9265930ea831a88ad99ce
xdpiqbx <xdpiqbx@gmail.com>
2022-07-02 21:23:00 +0300

    queue null test

---
+++ src/main/java/ua/goit/mod_008/lesson_001_Map_HashMap_LinkedHashMap/QueueNullTest.java
@@ -0,0 +1,12 @@
+package ua.goit.mod_008.lesson_001_Map_HashMap_LinkedHashMap;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class QueueNullTest {
+    public static void main(String[] args) {
+        Queue<Integer> numbers = new ArrayDeque<>();
+        // numbers.add(null); // NullPointerException
+        System.out.println("numbers.poll() = " + numbers.poll());
+    }
+}
