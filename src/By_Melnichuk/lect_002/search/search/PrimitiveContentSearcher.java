02d9df7221e248d41bca11ecfb61adc5d7ccd74b
xdpiqbx <xdpiqbx@gmail.com>
2022-07-15 07:54:00 +0300

    first part of lesson done

--- src/By_Melnichuk/lect_002/search/search/PrimitiveContentSearcher.java
+++ src/By_Melnichuk/lect_002/search/search/PrimitiveContentSearcher.java
@@ -1,9 +1,14 @@
 package By_Melnichuk.lect_002.search.search;
 
 public class PrimitiveContentSearcher implements ContentSearcher {
-
     @Override
     public boolean search(String content, String search) {
-        return content.toLowerCase().contains(search.toLowerCase());
+        String[] words = search.toLowerCase().split(" ");
+        for (String word : words) {
+            if(!content.contains(word)){
+                return false;
+            }
+        }
+        return true;
     }
 }
