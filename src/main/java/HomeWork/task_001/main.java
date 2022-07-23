b3f191c8dd266315bdf827452725362563dc4f5a
xdpiqbx <xdpiqbx@gmail.com>
2022-07-23 08:40:25 +0300

    task 1 in process

--- src/main/java/HomeWork/task_001/main.java
+++ src/main/java/HomeWork/task_001/main.java
@@ -1,8 +1,25 @@
 package HomeWork.task_001;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class main {
-    Collection<String> names = new ArrayList<>();
+    public static void main(String[] args) {
+
+        Collection<String> names = Arrays.asList("Will", "Stan", "John", "Bill", "Max", "Alex");
+
+        AtomicInteger i = new AtomicInteger();
+        Stream<String> namesStream = names
+                .stream()
+                .map(name -> {
+                    i.getAndIncrement();
+                    return i + ". " + name;
+                })
+                .filter(name -> Integer.parseInt(name.substring(0, 1)) % 2 != 0);
+
+        List<String> filteredNames = namesStream.collect(Collectors.toList());
+        System.out.println("filteredNames = " + filteredNames);
+    }
 }
