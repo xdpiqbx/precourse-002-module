7d5b3c1aef2c4eca0fd613a29fcb7a83acd1990f
xdpiqbx <xdpiqbx@gmail.com>
2022-07-24 12:50:57 +0300

    refactor + change task 3 flatMap

--- src/main/java/HomeWork/task_002/main.java
+++ src/main/java/HomeWork/task_002/main.java
@@ -15,6 +15,5 @@ public class main {
             .toList();
 
         System.out.println(sorted);
-
     }
 }
--- src/main/java/HomeWork/task_003/main.java
+++ src/main/java/HomeWork/task_003/main.java
@@ -3,24 +3,13 @@ package HomeWork.task_003;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class main {
     public static void main(String[] args) {
         String [] rawArr = {"1, 2, 0", "4, 5"};
 
-        String strNums = Arrays.stream(rawArr)
-                .reduce("",(str, element) -> {
-                    String [] arr = element.split(", ");
-                    StringBuilder strBuilder = new StringBuilder(str);
-                    for (String s : arr) {
-                        strBuilder.append(s);
-                    }
-                    str = strBuilder.toString();
-                    return str;
-                });
-
-        List <Integer> nums = Arrays.stream(strNums.split(""))
+        List <Integer> nums = Arrays.stream(rawArr)
+                .flatMap(element -> Arrays.stream(element.split(", ")))
                 .map(Integer::parseInt)
                 .sorted(Comparator.naturalOrder())
                 .toList();
